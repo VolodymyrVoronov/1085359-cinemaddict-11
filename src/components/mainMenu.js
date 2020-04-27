1d825dddad413d50f5e8302b38e6d7b2668fa6dc
Volodymyr <malones.vladimir@gmail.com>
2020-04-27 23:42:18 +0200

    adds method render in constructor PageController, adds function for sorting films and function for their rendering after sorting

--- src/components/mainMenu.js
+++ src/components/mainMenu.js
@@ -11,13 +11,7 @@ const createMainMenu = (filter) => {
       <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">${favorites}</span></a>
     </div>
     <a href="#stats" class="main-navigation__additional">Stats</a>
-  </nav>
-
-  <ul class="sort">
-    <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
-    <li><a href="#" class="sort__button">Sort by date</a></li>
-    <li><a href="#" class="sort__button">Sort by rating</a></li>
-  </ul>`);
+  </nav>`);
 };
 
 export default class MainMenuComponent extends AbstractComponent {
